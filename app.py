d65880b81d13b3449715bc31aafb37d6a2819ea4
Kamal Soni <124925313+kani-malso-webdev@users.noreply.github.com>
2023-05-09 04:07:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,11 @@
 from flask import Flask, request
-import firebase_admin
-from firebase_admin import credentials , db
 app = Flask(__name__)
 
-# cred = credentials.Certificate("serviceAccountKey.json")
-# firebase_admin.initialize_app(cred,{
 
-#     "databaseURL": "https://saathi-9eb76-default-rtdb.firebaseio.com"
-# })
-
-# ref1 = db.reference("test")
 @app.route("/", methods=["POST"])
 def index():
     output = request.get_json()
-    # ref1.set(output)
+   
 
     print(output)
 
